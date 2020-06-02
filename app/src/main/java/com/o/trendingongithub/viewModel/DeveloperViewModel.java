5f7047468c7b1fd2e53fbeac26e738a1bf0910a3
Vansh Arora <vansha10@gmail.com>
2020-06-03 00:30:14 +0530

    Code cleanup in viewmodels

--- app/src/main/java/com/o/trendingongithub/viewModel/DeveloperViewModel.java
+++ app/src/main/java/com/o/trendingongithub/viewModel/DeveloperViewModel.java
@@ -1,41 +1,29 @@
 package com.o.trendingongithub.viewModel;
 
 import androidx.lifecycle.LiveData;
-import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 import com.o.trendingongithub.dataRepository.DeveloperDataRepository;
 import com.o.trendingongithub.model.DeveloperData;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.inject.Inject;
 
 public class DeveloperViewModel extends ViewModel {
 
-    private MutableLiveData<List<DeveloperData>> mDeveloperData;
-    private DeveloperDataRepository mDeveloper;
-    private MutableLiveData<Boolean> isLoading = new MutableLiveData<>();
-
+    private DeveloperDataRepository mDeveloperRepository;
 
     @Inject
-    public DeveloperViewModel(DeveloperDataRepository mDeveloper) {
-        this.mDeveloper = mDeveloper;
-        mDeveloperData = mDeveloper.getDeveloperData();
+    public DeveloperViewModel(DeveloperDataRepository mDeveloperRepository) {
+        this.mDeveloperRepository = mDeveloperRepository;
     }
 
     public LiveData<List<DeveloperData>> getDeveloperData() {
-        if (mDeveloperData == null) {
-            List<DeveloperData> list = new ArrayList<>();
-            list.add(new DeveloperData("", "", "", ""));
-            mDeveloperData.setValue(list);
-        }
-        return mDeveloperData;
+        return mDeveloperRepository.getDeveloperData();
     }
 
     public LiveData<Boolean> getIsLoading() {
-        isLoading = mDeveloper.getIsLoading();
-        return isLoading;
+        return mDeveloperRepository.getIsLoading();
     }
 }
--- app/src/main/java/com/o/trendingongithub/viewModel/RepoViewModel.java
+++ app/src/main/java/com/o/trendingongithub/viewModel/RepoViewModel.java
@@ -1,7 +1,6 @@
 package com.o.trendingongithub.viewModel;
 
 import androidx.lifecycle.LiveData;
-import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 import com.o.trendingongithub.dataRepository.RepoDataRepository;
@@ -13,22 +12,18 @@ import javax.inject.Inject;
 
 public class RepoViewModel extends ViewModel {
 
-    private MutableLiveData<List<RepoData>> mRepoData;
     private RepoDataRepository mRepo;
-    private MutableLiveData<Boolean> isLoading = new MutableLiveData<>();
 
     @Inject
     public RepoViewModel(RepoDataRepository repoDataRepository) {
         this.mRepo = repoDataRepository;
-        mRepoData = mRepo.getRepoData();
     }
 
     public LiveData<List<RepoData>> getRepoData() {
-        return mRepoData;
+        return mRepo.getRepoData();
     }
 
     public LiveData<Boolean> getIsLoading() {
-        isLoading = mRepo.getIsLoading();
-        return isLoading;
+        return mRepo.getIsLoading();
     }
 }
